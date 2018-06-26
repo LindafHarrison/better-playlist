09eb6edf34562a5a8140fbdc6ec2267c598df345
Linda Harrison <lindafaithharrison@gmail.com>
2018-06-25 23:08:39 -0400

    minor change to button

--- src/App.js
+++ src/App.js
@@ -124,10 +124,9 @@ class App extends Component {
             )}
           </div>
           : <button onClick={() =>
-            window.location = window.location.includes('localhost')) {
-            ? 'http://localhost:8888/login'
-            : 'https://better-spotify-playlists.herokuapp.com/login'}
-        }
+            window.location = window.location.href.includes('localhost')
+              ? 'http://localhost:8888/login'
+              : 'https://better-spotify-playlists.herokuapp.com/login'}
             style={{ padding: '20px', 'fontSize': '90px', 'marginTop': '20px' }}>Sign in with Spotify</button>
         }
       </div>
